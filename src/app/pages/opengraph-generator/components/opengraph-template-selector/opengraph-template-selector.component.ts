2bced3eb22b016b48af56daf477e030ac84bfed4
Esteban Mayoral <contact@mpesteban.dev>
2025-04-08 16:22:27 +0200

    Remove references to swiper component

--- src/app/pages/opengraph-generator/components/opengraph-template-selector/opengraph-template-selector.component.ts
+++ src/app/pages/opengraph-generator/components/opengraph-template-selector/opengraph-template-selector.component.ts
@@ -1,12 +1,10 @@
 import { NgClass } from '@angular/common';
 import { Component, model, signal } from '@angular/core';
 import { TEMPLATES_TYPES, TemplateType } from '../../constants';
-import { SwiperCarrouselComponent } from '@/app/components/swiper/swiper-carrousel/swiper-carrousel.component';
-import { SwiperCarrouselItemComponent } from '@/app/components/swiper/swiper-carrousel-item/swiper-carrousel-item.component';
 
 @Component({
   selector: 'app-opengraph-template-selector',
-  imports: [NgClass, SwiperCarrouselComponent, SwiperCarrouselItemComponent],
+  imports: [NgClass],
   templateUrl: './opengraph-template-selector.component.html',
   styleUrl: './opengraph-template-selector.component.scss'
 })
