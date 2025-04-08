075fdc6738ba0c929ae9fa68c454ee0c5955981d
Esteban Mayoral <contact@mpesteban.dev>
2025-04-08 16:21:59 +0200

    Responsive choose template area.

--- src/app/pages/opengraph-generator/components/opengraph-template-selector/opengraph-template-selector.component.ts
+++ src/app/pages/opengraph-generator/components/opengraph-template-selector/opengraph-template-selector.component.ts
@@ -1,10 +1,12 @@
 import { NgClass } from '@angular/common';
 import { Component, model, signal } from '@angular/core';
 import { TEMPLATES_TYPES, TemplateType } from '../../constants';
+import { SwiperCarrouselComponent } from '@/app/components/swiper/swiper-carrousel/swiper-carrousel.component';
+import { SwiperCarrouselItemComponent } from '@/app/components/swiper/swiper-carrousel-item/swiper-carrousel-item.component';
 
 @Component({
   selector: 'app-opengraph-template-selector',
-  imports: [NgClass],
+  imports: [NgClass, SwiperCarrouselComponent, SwiperCarrouselItemComponent],
   templateUrl: './opengraph-template-selector.component.html',
   styleUrl: './opengraph-template-selector.component.scss'
 })
