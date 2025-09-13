ce5559188487cfe4253bd3e8e66655b892ecbc75
Esteban Mayoral <contact@mpesteban.dev>
2025-09-13 13:25:36 +0200

    Some cleanup on the monaco editor component

--- src/app/components/monaco-editor/monaco-editor.component.ts
+++ src/app/components/monaco-editor/monaco-editor.component.ts
@@ -1,5 +1,5 @@
 /// <reference path="../../../../node_modules/monaco-editor/monaco.d.ts" />
-import { Component, Input, AfterViewInit, ElementRef, ViewChild, Output, EventEmitter, OnChanges } from '@angular/core';
+import { Component, Input, AfterViewInit, ElementRef, ViewChild, Output, EventEmitter, OnChanges, OnDestroy, viewChild, model } from '@angular/core';
 
 let loadedMonaco = false;
 let loadPromise: Promise<void>;
@@ -9,9 +9,15 @@ let loadPromise: Promise<void>;
   templateUrl: './monaco-editor.component.html',
   styleUrls: ['./monaco-editor.component.scss']
 })
-export class MonacoEditorComponent implements AfterViewInit, OnChanges {
+export class MonacoEditorComponent implements AfterViewInit, OnChanges, OnDestroy {
 
-  @ViewChild('editorContainer') _editorContainer?: ElementRef;
+  //@ViewChild('editorContainer') _editorContainer?: ElementRef<HTMLDivElement>;
+  private readonly editorContainer = viewChild<ElementRef<HTMLDivElement>>('editorContainer');
+  readonly options = model<monaco.editor.IStandaloneEditorConstructionOptions | undefined>({
+    theme: 'vs-dark'
+  });
+
+  private monacoScriptTag?: HTMLScriptElement;
 
   @Input() code = '';
   @Output() codeChange = new EventEmitter<String>();
@@ -22,6 +28,9 @@ export class MonacoEditorComponent implements AfterViewInit, OnChanges {
   codeEditorInstance?: monaco.editor.IStandaloneCodeEditor;
 
   constructor() { }
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
 
   // supports two-way binding
   ngOnChanges() {
@@ -38,47 +47,49 @@ export class MonacoEditorComponent implements AfterViewInit, OnChanges {
       });
     } else {
       loadedMonaco = true;
-      loadPromise = new Promise<void>((resolve: any) => {
-        if (typeof ((<any>window).monaco) === 'object') {
-          resolve();
-          return;
-        }
-        const onAmdLoader: any = () => {
-          // Load monaco
-          (<any>window).require.config({ paths: { 'vs': 'assets/monaco/vs' } });
-
-          (<any>window).require(['vs/editor/editor.main'], () => {
-            this.initMonaco();
-            resolve();
-          });
-        };
-
-        // Load AMD loader if necessary
-        if (!(<any>window).require) {
-          const loaderScript: HTMLScriptElement = document.createElement('script');
-          loaderScript.type = 'text/javascript';
-          loaderScript.src = 'assets/monaco/vs/loader.js';
-          loaderScript.addEventListener('load', onAmdLoader);
-          document.body.appendChild(loaderScript);
-        } else {
-          onAmdLoader();
-        }
-      });
+      loadPromise = this.loadMonacoModule();
     }
   }
 
-  initMonaco(): void {
+  private loadMonacoModule(): Promise<void> {
+    return new Promise<void>((resolve: any) => {
+
+      if (typeof ((<any>window).monaco) === 'object') {
+        resolve();
+        return;
+      }
+
+      const onAmdLoader: any = () => {
+        // Load monaco
+        (<any>window).require.config({ paths: { 'vs': 'assets/monaco/vs' } });
+
+        (<any>window).require(['vs/editor/editor.main'], () => {
+          this.initMonaco();
+          resolve();
+        });
+      };
+
+      // Load AMD loader if necessary
+      if (!(<any>window).require) {
+        const loaderScript: HTMLScriptElement = document.createElement('script');
+        loaderScript.type = 'text/javascript';
+        loaderScript.src = 'assets/monaco/vs/loader.js';
+        loaderScript.addEventListener('load', onAmdLoader);
+        this.monacoScriptTag = loaderScript;
+        document.body.appendChild(loaderScript);
+      } else {
+        onAmdLoader();
+      }
+    });
+  }
+
+  private initMonaco(): void {
     // configure the monaco editor to understand custom language - customLang
     // monaco.languages.register(this.configService.getCustomLangExtensionPoint());
     // monaco.languages.setMonarchTokensProvider('CustomLang', this.configService.getCustomLangTokenProviders());
     // monaco.editor.defineTheme('customLangTheme', this.configService.getCustomLangTheme());   // add your custom theme here
 
-    this.codeEditorInstance = monaco.editor.create(this._editorContainer!.nativeElement, {
-      value: this.code,
-      language: 'typescript',
-      theme: 'vs-dark'
-      // theme: 'customLangTheme'
-    });
+    this.codeEditorInstance = monaco.editor.create(this.editorContainer()!.nativeElement, this.options());
 
     // To support two-way binding of the code
     this.codeEditorInstance!.getModel()!.onDidChangeContent(e => {
@@ -89,7 +100,8 @@ export class MonacoEditorComponent implements AfterViewInit, OnChanges {
     this.resizeObserver = new ResizeObserver(() => {
       this.codeEditorInstance!.layout();
     });
-    this.resizeObserver.observe(this._editorContainer!.nativeElement);
+
+    this.resizeObserver.observe(this.editorContainer()!.nativeElement);
   }
 
 }
